b82f51af2bebb524b42ea5f79938dcf200dde63b
Famelgas <filipe.damendes@gmail.com>
2021-11-14 19:21:42 +0000

    primeiras classes criadas

--- Product.java
+++ Product.java
@@ -1,3 +1,16 @@
 public class Product {
+    protected int identifier;
+    protected String name;
+    protected float unitPrice;
+    protected int stock;
+
+    public Product() {}
+
+    public Produtct(int identifier, String name, float unitPrice, int stock) {
+        this.identifier = identifier;
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.stock = stock;
+    }
 
 }
