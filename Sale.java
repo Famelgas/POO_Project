274ec81f0142371e05b811017fcf9f0b0db3ac17
Famelgas <filipe.damendes@gmail.com>
2021-11-14 19:51:42 +0000

    classes added

---
+++ Sale.java
@@ -0,0 +1,10 @@
+public class Sale {
+    // Each sale can be normal or have two types of promotion
+    private String type;
+    
+    public Sale() {}
+    
+    public Sale(String type) {
+        this.type = type;
+    }   
+}
