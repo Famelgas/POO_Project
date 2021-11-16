1d28201f4969f07736997ecbe87b3b2240ee575f
Famelgas <filipe.damendes@gmail.com>
2021-11-16 17:44:55 +0000

    class OnlineShoppingManager e onde esta a funçao main

---
+++ ManageDataBase.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList; 
+
+public class ManageDataBase {
+    // Imported client list from text file
+    ArrayList<Client> clientList;
+    // Imported supermarket protuct stock form text file 
+    ArrayList<Product> productList;
+
+    public ManageDataBase() {
+       clientList = new ArrayList<>();
+       productList = new ArrayList<>(); 
+    }
+    
+    // Import all the data from the text file to the corresponding ArrayLists
+    public void importFromTextFile() {}
+    
+}
---
+++ OnlineShppingManager.java
@@ -0,0 +1,5 @@
+public class OnlineShoppingManager {
+    public static void main(String[] args) {
+
+    }
+}
