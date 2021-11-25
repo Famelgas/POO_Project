fc00c7840f0e538c5a9e9e740511f34f6deb4fb7
Miguel Re <miguelremelo17@gmail.com>
2021-11-25 19:29:31 +0000

    up to date

--- src/promotion/PayLess.java
+++ src/promotion/PayLess.java
@@ -1,22 +1,34 @@
 package promotion;
+import java.util.ArrayList;
+import java.util.ArrayList;
 import product.*;
 
 public class PayLess extends Promotion {
     public PayLess() {}
-    
+
     public PayLess(String promotionType) {
         super(promotionType);
     }
 
-
-    public float priceCalculator(Product product) {
-        float finalPrice = 0;
-        int numberOfProducts = product.getStock();
-    
-        
-
-
+    public float priceCalculator(Product product){
+        ArrayList <Double> array;
+        double discount = 0.05;
+        double finalPrice = 0;
+        for (int i = 0; i < product.getStock(); i++ ){
+            if (product.getStock() == 1){
+                finalPrice = product.getUnitPrice();
+            }
+            else{
+                double discountedProduct = product.getUnitPrice() - (i*discount);
+                array.add(discountedProduct);
+            }
+        }
+        for (Double discountedProduct : array) {
+            finalPrice += discountedProduct;
+        }
         return finalPrice;
     }
+
+
 
 }
