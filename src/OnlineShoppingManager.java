ab77f83fcb57a839f37093962b7a29eca5cd10be
Miguel Re <miguelremelo17@gmail.com>
2021-11-26 12:51:12 +0000

    up to date

--- src/OnlineShoppingManager.java
+++ src/OnlineShoppingManager.java
@@ -1,49 +1,47 @@
+import java.sql.DatabaseMetaData;
 import java.util.Scanner;
+import database.DataBaseManager;
+import client.Client;
 
 public class OnlineShoppingManager {
     public static void main(String[] args) {
         int num = 0;
         int flag = 0;
-        while(flag  == 0){
-            System.out.println("choose one of the following options by selecting a number: ");
-            System.out.println("1. Login");
-            System.out.println("2. Create new account");
-            System.out.println("2. Create new account");
-            System.out.println("3. Make a purchase");
-            System.out.print("Your option: ");
-            Scanner sc = new Scanner(System.in);
-            num = sc.nextInt();
-    
-            if(num != 1 && num != 2 && num != 3){
-                System.out.println("Please, enter a valid option.");
+        Client client = new Client();
+        DataBaseManager dataBaseManager = new DataBaseManager();
+        while(true) {
+            while (flag == 0) {
+                System.out.println("        Object-Oriented Programming SuperMarket");
+                System.out.println("X------------------------------------------------------X");
+                System.out.println("                      1. Login");
+                System.out.println("                2. Create new account");
+                System.out.println("X------------------------------------------------------X");
+                System.out.print("Your option: ");
+                Scanner sc = new Scanner(System.in);
+                num = sc.nextInt();
+                if(num != 1 && num != 2){
+                    System.out.println();
+                    System.out.println("              Please, enter a valid option.");
+                    System.out.println();
+                }
             }
-            else{
-                flag = 1;
-            }
-            sc.close();
-        }  
+            
+            if(num == 1){   
+           
         
-        if(num == 1){   //onde vamos permitir realizar o login
-            String email = "";
-            System.out.print("Please, enter your e-mail: ");
-            Scanner sc = new Scanner(System.in);
-            email = sc.next();
-            
-
-        }
-
-        if(num == 2){  //onde vamos permitir fazer uma compra
 
 
 
-        }
+            }
+    
+            if(num == 2){  
+                if(createAccount() == true){
+                    client = dataBaseManager.createAccount();
+                }
 
-        if(num == 3){  //onde vamos permitir consultar as compras realizadas
 
 
-
-            
-        }
-
+            }
+        }     
     }   
 }
